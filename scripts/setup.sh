7b112b34c550eee19d7601c4466cf00ab5847c1c
mkan0141 <kanzume0141@gmail.com>
2023-05-03 00:21:03 +0900

    add setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SETUP_SCRIPT_DIR=$(dirname $0)
+
+bash "$SETUP_SCRIPT_DIR/setup-rust.sh"
+bash "$SETUP_SCRIPT_DIR/setup-homebrew.sh"
+bash "$SETUP_SCRIPT_DIR/setup-nvim.sh"
+
+echo -e "✅ \e[01;32mall setup success!!\e[m"
