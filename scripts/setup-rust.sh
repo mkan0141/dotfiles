bc1afb8499e88c9fe8aa68408c0256d2152a74cd
mkan0141 <kanzume0141@gmail.com>
2023-05-03 00:20:54 +0900

    add rust setup script

---
+++ scripts/setup-rust.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+function info () { echo -e "\e[35mrust setup =>\e[m $1"; }
+
+info "\e[34mstart rust setup...\e[m"
+
+# install rust
+if type "cargo" > /dev/null 2>&1; then
+  info "rust is already exists"
+else
+  info "install rust..."
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+info "update rust..."
+rustup update
+
+info "✅ \e[01;32msuccess rust setup !!\e[m\n"
